862c0a09c5bd7aed08caf82c55532ee0d4335a81
Alexey <alexeyushakov1997@yandex.ru>
2025-05-12 22:07:49 +0300

    feat: created useToggle()

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -1,5 +1,9 @@
+import { useToggle } from './hooks/useToggle';
+
 function Demo() {
-    return <></>;
+    const [value, toggle] = useToggle(['blue', 'orange', 'cyan', 'teal']);
+
+    return <button onClick={() => toggle()}>{value}</button>;
 }
 
 export default Demo;
